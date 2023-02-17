9e852528f032abbb73aacfb9cba3934e6f0a645b
Judson Cabral de Melo <judson.cabral.010@ufrn.edu.br>
2023-02-17 00:04:54 -0300

    feature: Criação dos comentários
    Criação da base do html do comentário.

---
+++ src/components/Comment.jsx
@@ -0,0 +1,37 @@
+import { Trash, ThumbsUp } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img
+        className={styles.avatar}
+        src="https://github.com/judsoncabral.png"
+      />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Judson Cabral</strong>
+              <time title="11 de Maio às 08:13" dateTime="2022-05-11 08:13:30">
+                Cerca de 2h atrás
+              </time>
+            </div>
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer className={styles.commentFooter}>
+          <button>
+            <ThumbsUp size={20} />
+            plaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
 export function Post(props) {
@@ -40,6 +41,12 @@ export function Post(props) {
           <button type="submit">Publicar</button>
         </footer>
       </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
     </article>
   );
 }
