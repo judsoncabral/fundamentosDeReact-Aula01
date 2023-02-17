57a858a119e20afa39763505fec33d453285b045
Judson Cabral de Melo <judson.cabral.010@ufrn.edu.br>
2023-02-16 23:21:17 -0300

    feature: Componente das postagem
    Criação da base do componente de postagem.

--- src/Post.jsx
+++
@@ -1,9 +0,0 @@
-export function Post(props) {
-  return (
-    <div>
-      <strong> {props.author}</strong>
-      <br></br>
-      <p>{props.content}</p>
-    </div>
-  );
-}
---
+++ src/components/Post.jsx
@@ -0,0 +1,45 @@
+import styles from "./Post.module.css";
+
+export function Post(props) {
+  return (
+    <article className={styles.post}>
+      <header className={styles.postHeader}>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/judsoncabral.png"
+          ></img>
+          <div className={styles.authorInfo}>
+            <strong>Judson Cabral de Melo</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="11 de Maio às 08:13" dateTime="2022-05-11 08:13:30">
+          Publicado há 1h
+        </time>
+      </header>
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          👉<a href="">jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="">#novoprojeto</a> <a href="">#nlw </a>
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+      <form className={styles.commentForm}>
+        <strong> Deixe seu feedback</strong>
+        <textarea placeholder="Deixe um comentário " id="comment" />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+    </article>
+  );
+}
