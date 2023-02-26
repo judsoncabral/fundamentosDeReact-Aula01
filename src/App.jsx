b97e7327d6e718dc72af17b28022df5fa640c55e
Judson Cabral de Melo <judson.cabral.010@ufrn.edu.br>
2023-02-26 15:24:21 -0300

    improve: Refatoração para post não ser estatico;
    Iniciando refatoração em relação ao post não ser estatico, colocando atributos de author,publishedAt,content. Faltando apenas a parte de comentario.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,56 @@ import { Post } from "./components/Post";
 import { Sidebar } from "./components/Sidebar";
 import "./global.css";
 import styles from "./App.module.css";
+
+const post = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/judsoncabral.png",
+      name: "Judson Cabral de Melo",
+      role: "Java Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz" +
+          "no Ignite do Rocketseat. O nome do projeto é Ignite Feed 🚀",
+      },
+      {
+        type: "link",
+        content: "https://github.com/judsoncabral",
+        linkName: "Judson Cabral github",
+      },
+    ],
+    publishedAt: new Date("2023-02-20 19:25:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/jeef-js.png",
+      name: "Jefferson Thiago",
+      role: "Web full stack developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz" +
+          "no Ignite do Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      {
+        type: "link",
+        content: "https://github.com/jeef-js",
+        linkName: "Jefferson Thiago github",
+      },
+    ],
+    publishedAt: new Date("2023-02-26 14:55:00"),
+  },
+];
+
 export function App() {
   return (
     <div>
@@ -10,14 +60,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Judson Cabral"
-            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae saepe maxime sed ipsam cupiditate aliquid. Itaque odio nesciunt id dolore odit esse pariatur magni, quibusdam hic tempora veniam dolorum explicabo?"
-          />
-          <Post
-            author="Filomena Cabral"
-            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae saepe maxime sed ipsam cupiditate aliquid. Itaque odio nesciunt id dolore odit esse pariatur magni, quibusdam hic tempora veniam dolorum explicabo?"
-          />
+          {post.map((post) => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
